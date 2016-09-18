289468cecadf6327a0130418662a637ea6a66fab
Vitaly Polisky <vpolisky@gmail.com>
2016-09-18 14:44:58 +0200

    Implement django feed spider command

---
+++ worldbrain/cortex/management/commands/feedspider.py
@@ -0,0 +1,22 @@
+import pika
+
+from django.core.management.base import BaseCommand
+
+from worldbrain.cortex.models import AllUrl, Source, SourceStates
+
+# Set up RabbitMQ connection
+SPIDER_QUEUE = 'worldbrain-spider'
+credentials = pika.PlainCredentials('worldbrain', 'worldbrain')
+parameters = pika.ConnectionParameters('polisky.me', 5672, '/worldbrain',
+                                       credentials)  # TODO use another server for RabbitMQ
+rabbitmq_connection = pika.adapters.blocking_connection.BlockingConnection(parameters)
+channel = rabbitmq_connection.channel()
+channel.queue_declare(queue=SPIDER_QUEUE)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        all_sources = Source.objects.all().filter(ready_for_crawling=True).filter(processed_spider='')
+        for source in all_sources:
+            channel.basic_publish(exchange='', routing_key=SPIDER_QUEUE, body=source.domain_name)
+        channel.close()
